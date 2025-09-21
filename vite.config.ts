785b3888af3cae829bb0fffba5c34225966bf654
pythondeveloper360@gmail.com <pythondeveloper360@gmail.com>
2025-09-21 12:45:52 +0500

    try

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-  build: {
-    outDir: "dist"
-  },
-  server: {
-    historyApiFallback: true
-  }
+  plugins: [react(),tailwindcss(  )],
+  base: "/"
 });
